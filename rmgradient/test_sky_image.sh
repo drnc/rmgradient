c7ec85207fb94aca22d331bab9140a9b558e4a39
Guillaume Duranceau <g2lm@drnc.net>
2019-03-06 07:14:23 +0100

    Remove output files in test_sky_image

--- rmgradient/test_sky_image.sh
+++ rmgradient/test_sky_image.sh
@@ -1,2 +1,3 @@
 #! /bin/sh
+rm -f test_data/sky_out.tif test_data/sky_background.tif
 cat test_data/skypoints | xargs python rmgradient.py -i test_data/sky.tif -o test_data/sky_out.tif -b test_data/sky_background.tif -s 3.0 -c 6 -p
